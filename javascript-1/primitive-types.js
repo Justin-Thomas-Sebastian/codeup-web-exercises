3389e6df2d612469f951620ceb8bb58a8465aebe
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-22 11:35:52 -0700

    edited primitive-types.js

--- javascript-1/primitive-types.js
+++ javascript-1/primitive-types.js
@@ -7,3 +7,14 @@ console.log(typeof '0');
 console.log(typeof 0);
 console.log(typeof true);
 console.log(typeof 'true');
+
+// What data type would best represent:
+
+/*
+A term or phrase typed into a search box? -> string
+If a user is logged in? -> boolean
+A discount amount to apply to a user's shopping cart? -> number
+Whether or not a coupon code is valid? -> boolean
+An email address typed into a registration form? -> string
+The price of a product? -> number
+ */
