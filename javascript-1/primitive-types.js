b5ae18c848f8f2b201cb6168446325d65c56b5e8
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-22 11:30:25 -0700

    edited primitive-types.js

--- javascript-1/primitive-types.js
+++ javascript-1/primitive-types.js
@@ -2,8 +2,8 @@ console.log(typeof 99.9);
 console.log(typeof "false");
 console.log(typeof false);
 console.log(typeof '0');
-console.log(0);
-console.log(true);
-console.log('true');
+console.log(typeof 0);
+console.log(typeof true);
+console.log(typeof 'true');
 
 
