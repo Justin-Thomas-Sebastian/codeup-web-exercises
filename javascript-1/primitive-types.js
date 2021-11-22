1ef7dff6d12affdca146bfbd58ca765740b6e096
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-22 11:33:02 -0700

    edited primitive-types.js

--- javascript-1/primitive-types.js
+++ javascript-1/primitive-types.js
@@ -1,3 +1,5 @@
+// Identify the data type of the following values:
+
 console.log(typeof 99.9);
 console.log(typeof "false");
 console.log(typeof false);
@@ -5,5 +7,3 @@ console.log(typeof '0');
 console.log(typeof 0);
 console.log(typeof true);
 console.log(typeof 'true');
-
-
