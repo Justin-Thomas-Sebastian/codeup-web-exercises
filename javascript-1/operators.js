0ddd321ce87c265721ae1993fb0083aee28977ce
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-22 11:50:10 -0700

    modified operators.js

---
+++ javascript-1/operators.js
@@ -0,0 +1,25 @@
+// For each of the following code blocks,
+// read the expression and predict what the result of evaluating it would be,
+// then execute the expression in the Chrome console.
+
+console.log('1' + 2);
+console.log(typeof '1' + 2);
+console.log(6 % 4);
+console.log('3 + 4 is ' + 3 + 4);
+// how could you make this produce the correct output?
+// modify it to -> '3 + 4 is ' + (3 + 4);
+
+console.log(0 < 0);
+console.log('false' == false);
+console.log(true == 'true');
+console.log(5 >= -5);
+console.log(!false || false);
+console.log(true || "42");
+console.log(!true && !false);
+console.log(6 % 5);
+console.log(5 < 4 && 1 === 1);
+console.log(typeof 'codeup' === 'string');
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+console.log(4 >= 0 && 1 !== '1');
+console.log(6 % 3 === 0);
+console.log(5 % 2 !== 0);
