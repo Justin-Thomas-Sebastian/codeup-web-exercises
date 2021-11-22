ddb87faa498c4b6563bf986b82f2dfa1d9fb5781
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-22 16:02:11 -0700

    added part 3

--- javascript-1/working-with-datatypes.js
+++ javascript-1/working-with-datatypes.js
@@ -114,13 +114,15 @@ console.log("Index of \"C\": " + sample.indexOf("C"));
 // 2.7 Retrieve a substring that contains only the word "Codeup" by using indexOf() and substring().
 let start = sample.indexOf("C");
 let end = sample.indexOf("p");
-console.log(start);
-console.log(end);
 console.log(sample.substring(start, end + 1));
 
 // 3. Write some JavaScript code, that is, variables and operators, to describe the following scenarios. 
 // Do not worry about the real operations to get the values, 
 // the goal of these exercises is to understand how real world conditions can be represented with code.
+
+console.log(" ");
+console.log("------- EXERCISE 3 -------");
+console.log(" ");
 
 // 3.1 You have rented some movies for your kids: The little mermaid (for 3 days), 
 // Brother Bear (for 5 days, they love it), 
@@ -132,8 +134,46 @@ let bear = 5;
 let hercules = 1;
 let rent = 3;
 let total = (mermaid + bear + hercules) * rent;
-console.log("Total rent: " + total);
+console.log("Total movie rent: $" + total);
 
+// 3.2. Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, 
+// they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. 
+// How much will you receive in payment for this week? You worked 10 hours for Facebook, 
+// 6 hours for Google and 4 hours for Amazon.
+
+let googleRate = 400;
+let amazonRate = 380;
+let facebookRate = 350;
+
+let googlePay = googleRate * 6;
+let amazonPay = amazonRate * 4;
+let facebookPay = facebookRate * 10;
+
+let totalPay = googlePay + amazonPay + facebookPay;
+console.log("Total pay is: $" + totalPay);
+
+// 3.3. A student can be enrolled in a class only if the class is not full 
+// and the class schedule does not conflict with her current schedule.
+
+let classNotFull = true;
+let noConflict = true;
+let canEnroll = classNotFull && noConflict;
+
+// 3.4. A product offer can be applied only if a person buys more than 2 items, 
+// and the offer has not expired. Premium members do not need to buy a specific amount of products.
+
+let numberInCart = 1;
+let promoExpired = false;
+let isPremium = true;
+let applyPromo = false;
+
+if(isPremium && !promoExpired){
+    applyPromo = true;
+} else if (promoExpired && numberInCart > 2){
+    applyPromo = true;
+} else {
+    applyPromo = false;
+}
 
 /*
 4. Create a variable that holds a boolean value for each of the following conditions:
