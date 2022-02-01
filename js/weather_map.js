50c26849694e21df57a84739b9d4041f5e67aa17
Justin Sebastian <justinthomassebastian@gmail.com>
2022-02-01 13:30:17 -0700

    added pin drop function on map. updates weather when dropped

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,12 +3,12 @@
 /**
  * TODOS:
  *
- * 1. 5-day forecast
+ * 1. 5-day forecast (DONE)
  * 2. Drop pin
  *      2.1. when user drops pin
  *      2.2  grab the pin's coordinates
  *      2.3  update weather (current and 5 day forecast)
- *  3. have location search update 5-day forecast as well
+ *  3. have location search update 5-day forecast as well (DONE)
  *  4. Update marker location based on new search input
  *  5. Finalize switch cases to change card bg image. Refer to https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
  *  6. Styling
@@ -17,70 +17,22 @@
 
 /***************   PROGRAM EXECUTION  *******************/
 
-// DEFAULT WEATHER VIEW
-getCurrentWeather("El Paso");
-getForecast("El Paso");
+// El Paso Coordinates
+const originLong = -106.2005;
+const originLat = 31.8001;
+
+// Default Weather View (El Paso)
+getCurrentWeather(originLong, originLat);
+getForecast(originLong, originLat);
 
 /***************   WEATHER MAP SECTION  *******************/
 
-// Get weather forecast from passed in location string
-function getForecast(search){
-    $.get("http://api.openweathermap.org/data/2.5/forecast", {
-        APPID: WEATHER_KEY,
-        q: search
-    }).done(function(result){
-
-        console.log(result.list[0]);
-
-        let currentTime = Date.now();
-        let currentTimeStr = currentTime.toString().slice(0,-3); // remove last three digits to match convention in openweather api
-        let unixTime24HoursFromNow = getNextDayUnixTime(currentTimeStr); // adds 24 hours to current time
-        let dayCount = 1;  // keeps track of how many days we want to forecast
-
-        // loop through result list
-        for(let forecastObj of result.list){
-
-            if(dayCount === 5){  // should be 5 just testing 1 card for now
-                break;  // exit once we hit 5 days
-            }
-
-            // find most recent forecast that is closest to 24 hours from now
-            if(forecastObj.dt >= unixTime24HoursFromNow){
-                console.log(formatUnixDate(forecastObj.dt));
-                console.log(formatUnixTime(forecastObj.dt));
-                console.log(" ");
-                unixTime24HoursFromNow = getNextDayUnixTime(unixTime24HoursFromNow);  // next 24 hours
-                populateForecast(forecastObj, dayCount);
-                dayCount++;
-            }
-        }
-    }).fail(function(){
-        console.log("Failed to retrive data");
-    });
-}
-
-// renders data to forecast cards
-function populateForecast(forecastObj,currentDay){
-    // CARD HEADER
-    $(`#forecast-date-${currentDay}`).text(formatUnixDate(forecastObj.dt));
-    $(`#forecast-time-${currentDay}`).text(formatUnixTime(forecastObj.dt));
-
-    // CARD BODY
-    $(`#forecast-high-${currentDay}`).text(forecastObj.main.temp_max);
-    $(`#forecast-low-${currentDay}`).text(forecastObj.main.temp_min)
-
-    // CARD LIST
-    $(`#forecast-details-${currentDay} li:nth-child(1)`).text("Description: " + forecastObj.weather[0].description);
-    $(`#forecast-details-${currentDay} li:nth-child(2)`).text("Humidity: " + forecastObj.main.humidity);
-    $(`#forecast-details-${currentDay} li:nth-child(3)`).text("Wind Speed: " + forecastObj.wind.speed);
-    $(`#forecast-details-${currentDay} li:nth-child(4)`).text("Pressure: " + forecastObj.main.pressure);
-}
-
 // Get current weather in passed in location string
-function getCurrentWeather(search){
+function getCurrentWeather(inputLong, inputLat){
     $.get("http://api.openweathermap.org/data/2.5/weather", {
         APPID: WEATHER_KEY,
-        q: search,
+        lon: inputLong,
+        lat: inputLat,
         units: "imperial"
     }).done(function (result) {
 
@@ -104,9 +56,60 @@ function getCurrentWeather(search){
 
         // Render card body bg image
         renderWeatherBackgroundImage(result.weather[0].main);
-
-        // console.log(result);
     });
+}
+
+// Get weather forecast from passed in location string
+function getForecast(inputLong, inputLat){
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: WEATHER_KEY,
+        lon: inputLong,
+        lat: inputLat,
+        units: "imperial"
+    }).done(function(result){
+
+        let currentTime = Date.now();
+        let currentTimeStr = currentTime.toString().slice(0,-3); // remove last three digits to match convention in openweather api
+        let unixTime24HoursFromNow = getNextDayUnixTime(currentTimeStr); // adds 24 hours to current time
+        let dayCount = 1;  // keeps track of how many days we want to forecast
+
+        // loop through result list
+        for(let forecastObj of result.list){
+
+            if(dayCount === 5){
+                break;  // exit once we hit 5 days
+            }
+
+            // find most recent forecast that is closest to 24 hours from now
+            if(forecastObj.dt >= unixTime24HoursFromNow){
+                populateForecast(forecastObj, dayCount);
+                unixTime24HoursFromNow = getNextDayUnixTime(unixTime24HoursFromNow);  // next 24 hours
+                dayCount++;
+            }
+        }
+    }).fail(function(){
+        console.log("Failed to retrieve data");
+    });
+}
+
+// renders data to forecast cards
+function populateForecast(forecastObj,currentDay){
+    // CARD HEADER
+    $(`#forecast-date-${currentDay}`).text(formatUnixDate(forecastObj.dt));
+    $(`#forecast-time-${currentDay}`).text(formatUnixTime(forecastObj.dt));
+
+    // CARD BODY
+    $(`#forecast-high-${currentDay}`).text("H: " + forecastObj.main.temp_max + "\u2109");
+    $(`#forecast-low-${currentDay}`).text("L: " + forecastObj.main.temp_min + "\u2109");
+
+    let icon = forecastObj.weather[0].icon;
+    $(`#forecast-icon-${currentDay}`).attr("src",`http://openweathermap.org/img/w/${icon}.png`);
+
+    // CARD LIST
+    $(`#forecast-details-${currentDay} li:nth-child(1)`).text("Description: " + forecastObj.weather[0].description);
+    $(`#forecast-details-${currentDay} li:nth-child(2)`).text("Humidity: " + forecastObj.main.humidity);
+    $(`#forecast-details-${currentDay} li:nth-child(3)`).text("Wind Speed: " + forecastObj.wind.speed);
+    $(`#forecast-details-${currentDay} li:nth-child(4)`).text("Pressure: " + forecastObj.main.pressure);
 }
 
 // Adds appropriate bg image based on weather description
@@ -115,6 +118,11 @@ function renderWeatherBackgroundImage(weather){
     switch(weather){
         case "Clouds":
             cardBodyBg.css("background-image", "url(../assets/cloudy.jpg)");
+            cardBodyBg.css("color", "black");
+            break;
+        case "Clear":
+            cardBodyBg.css("background-image", "url(../assets/clear.jpg)");
+            cardBodyBg.css("color", "white");
             break;
         default:
             cardBodyBg.css("background-image", "");
@@ -125,7 +133,7 @@ function renderWeatherBackgroundImage(weather){
 /*****************  MAPBOX SECTION  **********************/
 
 mapboxgl.accessToken = MAPBOX_KEY;
-let markers = [];  // global array which holds all markers
+let currentMarker = [];  // holds current marker. only allowed to contain one at a time
 
 // create map
 let map = new mapboxgl.Map({
@@ -142,16 +150,41 @@ $("#search-btn").click(function () {
         map.setZoom(10);
         map.setCenter(results);
 
-        let marker = new mapboxgl.Marker()
+        if(currentMarker.length > 0){
+            let removeMarker = currentMarker.pop();
+            removeMarker.remove();
+        }
+
+        let newMarker = new mapboxgl.Marker()
             .setLngLat([results[0], results[1]])
             .addTo(map);
 
-        // add to global markers array
-        markers.push(marker);
-    });
+        // assign as current marker
+        currentMarker.push(newMarker);
 
-    getCurrentWeather(search);  // update current weather card
-    getForecast(search);  // update forecast row
+        getCurrentWeather(results[0], results[1]);  // update current weather card
+        getForecast(results[0], results[1]);  // update forecast row
+    });
+});
+
+// adds marker on map on click and updates weather
+map.on('click', function(e) {
+    let coordinates = e.lngLat;
+    console.log(coordinates);
+
+    if(currentMarker.length > 0){
+        let removeMarker = currentMarker.pop();
+        removeMarker.remove();
+    }
+
+    let newMarker = new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .addTo(map);
+
+    currentMarker.push(newMarker);
+
+    getCurrentWeather(coordinates.lng, coordinates.lat);
+    getForecast(coordinates.lng, coordinates.lat);
 });
 
 /********************  UTILITIES  *****************************/
