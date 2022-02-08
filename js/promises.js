d508bd93412510440385ab15ca03cd635d9de02c
Justin Sebastian <justinthomassebastian@gmail.com>
2022-02-08 13:16:30 -0700

    finished promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,13 +1,16 @@
 "use strict";
 
-function getLastCommit(username){
+// Create a function that accepts a GitHub username,
+// and returns a promise that resolves
+// returning just the date of the last commit that user made.
+function getLastCommit(username) {
     return fetch(`https://api.github.com/users/${username}/events`,
         {headers: {'Authorization': GITHUB_TOKEN}})
         .then(response => response.json())
-        .then( events => {
+        .then(events => {
             let pushes = events.filter(event => event.type = "PushEvent");
 
-            let lastCommit = pushes.reduce( (last, current) => {
+            let lastCommit = pushes.reduce((last, current) => {
                 let currentDate = new Date(last.created_at);
                 let nextDate = new Date(current.created_at);
                 return currentDate.getTime() >= nextDate.getTime() ? last : current;
@@ -16,26 +19,30 @@ function getLastCommit(username){
             return new Date(lastCommit.created_at);
         })
         .catch(error => {
-           console.log("Error: " + error);
+            console.log("Error: " + error);
         });
 }
 
+// DOM Elements
 let findCommitBtn = document.getElementById("find-commit-btn");
 let githubUsername = document.getElementById("github-username");
 let commitResult = document.getElementById("github-latest-commit-result");
 
+// Find last commit in passed in username
 findCommitBtn.addEventListener("click", () => {
-    getLastCommit(githubUsername.value).then(result =>{
+    getLastCommit(githubUsername.value).then(result => {
         commitResult.innerText = String(result);
     });
 });
 
-function wait(duration){
-    return new Promise( (resolve, reject) => {
+// Write a function named wait that accepts a number as a parameter,
+// and returns a promise that resolves after the passed number of milliseconds.
+function wait(duration) {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if(!isNaN(duration) && duration === 1000) {
+            if (!isNaN(duration) && duration === 1000) {
                 resolve(`You'll see this after ${duration / 1000} second`);
-            } else if(!isNaN(duration)){
+            } else if (!isNaN(duration)) {
                 resolve(`You'll see this after ${duration / 1000} seconds`);
             } else {
                 reject("Type error. Enter number in milliseconds.");
